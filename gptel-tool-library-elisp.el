e978b05a191d37eb66061b076e93c5fadb9e56fa
Bernd Wachter <bernd-github@wachter.fi>
2025-06-01 23:21:14 +0300

    feat: Add functions for Elisp variable and function docs
    This commit adds new functions that retrieve documentation for Elisp
    variables and functions, describe symbols, and provide fuzzy matching
    for symbol names.
    
    - `gptel-tool-library-elisp-variable-doc`: Returns documentation
      for a given variable name. If the variable does not exist, it
      returns a message indicating no documentation was found.
    
    - `gptel-tool-library-elisp-function-doc`: Returns documentation for
      a given function name. If the function does not exist, it provides
      an appropriate message.
    
    - `gptel-tool-library-elisp-describe-symbol`: Retrieves source code
      or the sexp of a function or variable if the source is not found.
    
    - `gptel-tool-library-elisp-describe-symbol-fuzzy`: Lists functions
      and variables that match a given name fuzzily, with an optional
      limit on the number of results.
    
    These utilities enhance the library's ability to introspect Elisp
    symbols, facilitating better documentation retrieval and analysis.

--- gptel-tool-library-elisp.el
+++ gptel-tool-library-elisp.el
@@ -135,5 +135,118 @@ If BUFFER is nil, use the current buffer."
                      :description "The buffer to perform the replacement"))
  :category "elisp")
 
+(defun gptel-tool-library-elisp-variable-doc (name)
+  "Try to return documentation for variable NAME"
+  (gptel-tool-library--debug-log (format "elisp-variable-doc: %s" name))
+  (let ((var (intern-soft name)))
+    (if (and var (boundp var))
+        (documentation-property var 'variable-documentation)
+      (format "No documentation found for variable: %s" name))))
+
+(gptel-tool-library-make-tools-and-register
+ 'gptel-tool-library-elisp-tools
+ :function #'gptel-tool-library-elisp-variable-doc
+ :name "elisp-variable-doc"
+ :category "elisp"
+ :description "Retrieve the documentation for an Elisp variable."
+ :args (list '(:name "name"
+                     :type "string"
+                     :description "The name of the variable to retrieve documentation for.")))
+
+(defun gptel-tool-library-elisp-function-doc (name)
+  "Try to return documentation for function NAME"
+    (gptel-tool-library--debug-log (format "elisp-function-doc: %s" name))
+    (let ((func (intern-soft name)))
+      (if (and func (fboundp func))
+          (documentation func)
+        (format "No documentation found for function: %s" name))))
+
+(gptel-tool-library-make-tools-and-register
+ 'gptel-tool-library-elisp-tools
+ :function #'gptel-tool-library-elisp-function-doc
+ :name "elisp-function-doc"
+ :category "elisp"
+ :description "Retrieve the documentation for an Elisp function."
+ :args (list '(:name "name"
+                     :type "string"
+                     :description "The name of the function to retrieve documentation for.")))
+
+(defun gptel-tool-library-elisp-describe-symbol (name)
+  "Return the source code for function or variable NAME as a string.
+If source isn't found, falls back to the Emacs Lisp object sexp."
+       (let* ((sym (if (symbolp name) name (intern name)))
+              (callable (or (functionp sym) (macrop sym)))
+              (find-fn (if callable #'find-function-noselect #'find-variable-noselect)))
+         (condition-case nil
+             (let* ((res (funcall find-fn sym))
+                    (buf (car res))
+                    (pos (cdr res)))
+               (with-current-buffer buf
+                 (save-excursion
+                   (goto-char pos)
+                   (buffer-substring-no-properties
+                    (point)
+                    (progn (end-of-defun) (point))))))
+           (error
+            (let ((obj (if callable
+                           (symbol-function sym)
+                         (symbol-value sym))))
+              (pp-to-string obj))))))
+
+(gptel-tool-library-make-tools-and-register
+ 'gptel-tool-library-elisp-tools
+ :function #'gptel-tool-library-elisp-describe-symbol
+ :name "elisp-describe-symbol"
+ :category "elisp"
+ :description "Return source code for function or variable."
+ :args (list '(:name "name"
+                     :type "string"
+                     :description "The name of the symbol to receive sources for.")))
+
+(defun gptel-tool-library-elisp-describe-symbol-fuzzy (name &optional limit)
+  "List all functions and variables that fuzzily match the given name, along with a one-sentence summary, with an optional limit on the number of results."
+  (lambda (name &optional limit)
+    (gptel-tool-library--debug-log (format "elisp_fuzzy_match: %s, limit: %s" name limit))
+    (let ((matches '()))
+      ;; Search functions
+      (mapatoms (lambda (sym)
+                  (when (and (fboundp sym) (string-match-p name (symbol-name sym)))
+                    (push (format "Function: %s - %s"
+                                  (symbol-name sym)
+                                  (or (ignore-errors
+                                        (documentation sym))
+                                      "No documentation available."))
+                          matches))))
+      ;; Search variables
+      (mapatoms (lambda (sym)
+                  (when (and (boundp sym) (string-match-p name (symbol-name sym)))
+                    (push (format "Variable: %s - %s"
+                                  (symbol-name sym)
+                                  (or (ignore-errors
+                                        (documentation-property sym 'variable-documentation))
+                                      "No documentation available."))
+                          matches))))
+      ;; Apply limit if specified
+      (when (and limit (> (length matches) limit))
+        (setq matches (seq-take matches limit)))
+      (if matches
+          (string-join matches "
+")
+        (format "No matches found for name: %s" name)))))
+
+(gptel-tool-library-make-tools-and-register
+ 'gptel-tool-library-elisp-tools
+ :function #'gptel-tool-library-elisp-describe-symbol-fuzzy
+ :name "elisp-fuzzy-match"
+ :category "elisp"
+ :description "List all functions and variables that fuzzily match the given name, along with a one-sentence summary, with an optional limit on the number of results."
+ :args (list '(:name "name"
+                     :type "string"
+                     :description "The name to match against available functions and variables.")
+             '(:name "limit"
+                     :type "integer"
+                     :optional t
+                     :description "The maximum number of results to return. If nil, returns all matches.")))
+
 (provide 'gptel-tool-library-elisp)
 ;;; gptel-tool-library-elisp.el ends here
